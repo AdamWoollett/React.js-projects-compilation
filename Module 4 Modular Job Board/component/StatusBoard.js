f6497099e193f767f71ec0a8ccd703234c68b7c0
AdamWoollett <adam.woollett@btinternet.com>
2026-05-15 15:54:24 +0100

    Update StatusBoard.js

--- Module 4 Modular Job Board/component/StatusBoard.js
+++ Module 4 Modular Job Board/component/StatusBoard.js
@@ -7,13 +7,13 @@ import TicketInfo from './TicketInfo.js'
 const StatusBoard = () => {
     return (
         <div className="StatusBoard">
-            <TicketInfo Image={CompletedImage} TicketStatus="Completed" TicketCount="10" TicketInfo="Jess, Bob, Jerry">
+            <TicketInfo Image={CompletedImage} TicketStatus="Completed" TicketCount="10" TicketInfo="Jess, Bob, Jerry, Thomas, Steve, Alex, William, Michael, Sam, Isaac">
                 <p>Tickets that have been completed</p>
             </TicketInfo>
-            <TicketInfo Image={InProgressImage} TicketStatus="InProgress" TicketCount="5" TicketInfo="Walker, Adams, Lugo">
+            <TicketInfo Image={InProgressImage} TicketStatus="InProgress" TicketCount="5" TicketInfo="Walker, Adams, Lugo, Konrad, Riggs">
                 <p>Tickets currently in progress</p>
             </TicketInfo>
-            <TicketInfo Image={FailedImage} TicketStatus="Failed" TicketCount="2" TicketInfo="Garfield, Odie, Jon">
+            <TicketInfo Image={FailedImage} TicketStatus="Failed" TicketCount="2" TicketInfo="Garfield, Jon">
                 <p>Tickets that have failed</p>
             </TicketInfo>
         </div>
